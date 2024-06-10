ac1a612a01b3d83cfe394042f6579dcd02710c67
Ankush9140 <158093787+Ankush9140@users.noreply.github.com>
2024-06-10 14:13:07 +0530

    Update SEARCH.jsx

--- Project1/src/Components/SEARCH.jsx
+++ Project1/src/Components/SEARCH.jsx
@@ -1,16 +1,24 @@
-export default function SEARCH({flag,setFlag,location,setLocation,color}) {
+export default function SEARCH({ flag, setFlag, location, setLocation, color }) {
     return (
+        // Container div
         <div className="text-center p-4 ml-5 md:ml-14">
             <input
+                {/* Input value bound to the location state */}
                 value={location}
+                {/* Update location state on input change */}
                 onChange={event => setLocation(event.target.value)}
+                {/* Handle key down events */}
                 onKeyDown={function (event) {
-                    if (event.key == 'Enter') {
+                    if (event.key === 'Enter') {
+                        {/* Toggle the flag state on Enter key press to trigger data fetching */}
                         setFlag(!flag);
                     }
                 }}
+                {/* Placeholder text for the input field */}
                 placeholder='Find Weather'
+                {/* Set input type to text */}
                 type='text'
+                {/* Styling */}
                 className={`p-2 md:p-4 lg:p-6
                         text-lg md:text-xl h-9
                         rounded-2xl
